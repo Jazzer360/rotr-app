74e9d4ccb6dfcfdfb5887505841f5114d0a3f3d2
Derek Jass <jazzer36@gmail.com>
2025-06-25 14:24:45 -0500

    Remove reflex badge.

--- rxconfig.py
+++ rxconfig.py
@@ -6,5 +6,6 @@ import reflex as rx
 config = rx.Config(
     app_name="rotr_app",
     redis_url=os.getenv('REDIS_URL'),
-    plugins=[rx.plugins.TailwindV3Plugin()]
+    plugins=[rx.plugins.TailwindV3Plugin()],
+    show_built_with_reflex=False
 )
