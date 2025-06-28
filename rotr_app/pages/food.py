20ee846497f25df1c0d8eb4ec8248803817c8f1c
Derek Jass <jazzer36@gmail.com>
2025-06-28 14:26:04 -0500

    Added vendors.

--- rotr_app/pages/food.py
+++ rotr_app/pages/food.py
@@ -6,6 +6,23 @@ from ..template import template
 
 vendors = [
     {
+        'vendor': 'Cheese Carriage',
+        'menu': [
+            {
+                'item': 'Cheese Curds',
+                'price': '$8'
+            },
+            {
+                'item': 'Corn Dog',
+                'price': '$6'
+            },
+            {
+                'item': 'Pop & Water',
+                'price': '$3'
+            }
+        ]
+    },
+    {
         'vendor': 'Cup \'N Saucer',
         'menu': [
             {
@@ -15,10 +32,77 @@ vendors = [
         ]
     },
     {
-        'vendor': 'Cheese Carriage',
+        'vendor': "Ole's Gathering Place",
         'menu': [
             {
-                'item': 'Cheese Curds'
+                'item': 'Smoked Pork Sandwich',
+                'price': '$8',
+                'options': [
+                    {
+                        'option': 'w/ coleslaw, smoked baked beans',
+                        'price': '$12'
+                    }
+                ]
+            },
+            {
+                'item': 'Smoked Brisket Sandwich',
+                'price': '$10',
+                'options': [
+                    {
+                        'option': 'w/ coleslaw, smoked baked beans',
+                        'price': '$14'
+                    }
+                ]
+            },
+            {
+                'item': 'Queso Bacon Mac and Cheese',
+                'price': '$10',
+                'options': [
+                    {
+                        'option': 'w/ pork',
+                        'price': '$12'
+                    },
+                    {
+                        'option': 'w/ brisket',
+                        'price': '$14'
+                    }
+                ]
+            },
+            {
+                'item': 'Queso Nachos (onion, tomato, jalapeno)',
+                'price': '$10',
+                'options': [
+                    {
+                        'option': 'w/ pork',
+                        'price': '$12'
+                    },
+                    {
+                        'option': 'w/ brisket',
+                        'price': '$14'
+                    }
+                ]
+            },
+            {
+                'item': 'Oledelphia (pork)',
+                'price': '$12'
+            },
+            {
+                'item': 'Oledelphia (brisket)',
+                'price': '$14',
+                'options': [
+                    {
+                        'option': 'w/ coleslaw, smoked baked beans',
+                        'price': '$16'
+                    }
+                ]
+            },
+            {
+                'item': 'Side of colelsaw, smoked baked beans',
+                'price': '$3'
+            },
+            {
+                'item': 'Pepsi, Mt. Dew, Dr. Pepper, Diet Dr. Pepper',
+                'price': '$1'
             }
         ]
     }
@@ -26,22 +110,41 @@ vendors = [
 
 
 def menu_item(item: dict) -> rx.Component:
-    return [
-        rx.text(item.get('item'), size='3'),
+    if 'options' in item:
+        options = [menu_option(option) for option in item.get('options')]
+    else:
+        options = None
+    desc = item['desc'] if 'desc' in item else None
+    return rx.box(
+        rx.hstack(
+            rx.text(item.get('item'), size='3'),
+            rx.spacer(),
+            rx.text(item.get('price'), size='3')
+        ),
+        options,
         rx.text(
-            item.get('desc'),
+            desc,
             size='2',
             margin_left='2em',
             font_style='italic'
-        )
-    ]
+        ),
+        width='100%'
+    )
+
+
+def menu_option(option: dict):
+    return rx.hstack(
+        rx.text(option.get('option'), margin_left='1em'),
+        rx.spacer(),
+        rx.text(option.get('price'))        
+    )
 
 
 def vendor_item(menu: dict) -> rx.Component:
     return rx.container(
         rx.vstack(
-            rx.heading(menu.get('vendor')),
             rx.card(
+                rx.heading(menu.get('vendor'), margin_bottom='1em'),
                 rx.vstack(
                     [menu_item(item) for item in menu.get('menu')]
                 ),
@@ -50,7 +153,8 @@ def vendor_item(menu: dict) -> rx.Component:
             align='center'
         ),
         size='1',
-        padding='8px'
+        padding='8px',
+        width='100%'
     )
 
 
@@ -60,8 +164,8 @@ def vendor_item(menu: dict) -> rx.Component:
     on_load=NavState.update)
 @template
 def schedule() -> rx.Component:
-    return rx.vstack(
-        [rx.box(vendor_item(vendor), width='100%') for vendor in vendors],
+    return rx.box(
+        [vendor_item(vendor) for vendor in vendors],
         width='100%',
         align='center'
     )
