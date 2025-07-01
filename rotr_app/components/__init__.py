b95f80dd8a0097c5c71bf4489d5e572157545cb2
Derek Jass <jazzer36@gmail.com>
2025-07-01 14:31:54 -0500

    Fixed what refactoring broke

--- rotr_app/components/__init__.py
+++ rotr_app/components/__init__.py
@@ -2,4 +2,4 @@ from .footer import footer
 from .navbar import navbar
 from .surveypopup import survey_popup
 
-__all__ = ["footer", "navbar", "surveypopup"]
+__all__ = ["footer", "navbar", "survey_popup"]
